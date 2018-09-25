5ba24536949ca63cd91d26e9aef35eca5eae8adb
bbeyt <bbeyt@trinity.edu>
2018-09-25 12:32:53 -0500

    Changed map header's left button to a menu button. Changed app icon to trinityEmblem.

--- TU-Way/App.js
+++ TU-Way/App.js
@@ -3,7 +3,6 @@ import { createStackNavigator } from 'react-navigation';
 import LoginScreen from './LoginScreen';
 import MapScreen from './MapScreen';
 
-
 const RootStack = createStackNavigator(
   {
     Login: {
