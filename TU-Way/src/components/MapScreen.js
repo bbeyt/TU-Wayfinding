56b27894b1072e2cfae7f43fc0f16102c63e20eb
JVman0071234 <jviltoft@trinity.edu>
2018-11-06 10:59:27 -0600

    Added Axios and Coord List

--- TU-Way/src/components/MapScreen.js
+++ TU-Way/src/components/MapScreen.js
@@ -4,6 +4,37 @@ import { MapView } from 'expo';
 import { Container, Header, Right, Body, Left, Button, Icon, } from 'native-base';
 import { SearchBar } from 'react-native-elements';
 import SearchInput, { createFilter } from 'react-native-search-filter';
+import Geocoder from 'react-native-geocoding';
+
+const axios = require('axios');
+
+const baseURL = "https://maps.googleapis.com/maps/api/directions/json";
+const originLatLong = "29.460987,-98.482356";
+const destinationLatLong = "29.463259,-98.482488";
+const apiKey = "AIzaSyBWZJ_hTM78RKil6GW-aBtqOf0DoNWwmcY";
+const dirMode = "walking";
+
+axios.get(baseURL, {
+    params: {
+        origin: originLatLong,
+        destination: destinationLatLong,
+        key: apiKey,
+        mode: dirMode
+    }
+})
+
+Geocoder.init(apiKey);
+
+//Order is "Name": ["Latitude,Longitude"]
+
+var nameToCoordinate = {"Mabee": ["29.460987,-98.482356"], "WBC": ["29.459727,-98.483617"], "WITT": ["29.461367,-98.482488"], 
+"COAT": ["29.462540,-98.482050", "29.462747,-98.482069", "29.462818,-98.482171", "29.462796,-98.482495", "29.462911,-98.482496"], 
+"Community and Campus Involvement": ["29.462773,-98.482756"], "NH": ["29.463259,-98.482488", "29.463255,-98.482792"], 
+"Financial Services": ["29.463317,-98.482430"], "Tower": ["29.462911,-98.483532"], "MPC": ["29.463027,-98.483903", "29.462915,-98.484398"], 
+"SML": ["29.462498,-98.483948", "29.462466,-98.484082"], "RTT": ["29.463746,-98.482417"], "DSB": ["29.463619,-98.481803", "29.463748,-98.482294"], 
+"LA": ["29.464673,-98.481816", "29.464186,-98.482196"], "Library": ["29.464903,-98.483136"], "HAS": ["29.465646,-98.483863"], 
+"CGC": ["29.465046,-98.484164", "29.464955,-98.483885", "29.465421,-98.483883"], 
+"CSI": ["29.463982,-98.483790", "29.464406,-98.483688", "29.464784,-98.483875"]};
 
 //Get height and width of screen
 var { height, width } = Dimensions.get('window');
